9647c1b15d12469adb5da3c9593162d9230114df
Jonne Okkonen <M2235@Labranet.jamk.fi>
2019-04-17 11:28:29 +0300

    Core: Create QueryBuilder
    Create class that can be used to create query strings.
    
    Add QueryBuilder class
    Add Field class
    Create UnitTestsProject
    Create UnitTests for QueryBuilder

---
+++ FinlandVehicleRegister/Core/QueryBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinlandVehicleRegister.Core
+{
+    public class QueryBuilder
+    {
+        /// <summary>
+        /// QueryString that was created in build
+        /// </summary>
+        public string QueryString
+        {
+            get
+            {
+                return queryString;
+            }
+        }
+        private string queryString { get; set; }
+        private readonly string TableName = "Ajoneuvo";
+        private List<Field> fields = new List<Field>();
+        public enum QueryType
+        {
+            Select
+        }
+        
+        /// <summary>
+        /// Builds Basic Query based on query type and given fields
+        /// </summary>
+        /// <param name="type"></param>
+        public void Build(QueryType type)
+        {
+            switch(type)
+            {
+                case QueryType.Select:
+                    queryString += $"SELECT * FROM {TableName}";
+                    if (fields.Count != 0)
+                    {
+                        queryString += " WHERE ";
+                        Field last = fields.Last();
+                        foreach (Field f in fields)
+                        {
+                            if (f == last)
+                                queryString += $"{f.FieldName}='{f.Value}';";
+                            else
+                                queryString += $"{f.FieldName}='{f.Value}', ";
+                        }
+                    }
+                    else queryString += ";";
+                    break;   
+            }
+        }
+
+        public void AddField(Field.Fields field, string value)
+        {
+            fields.Add(new Field(field, value));
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinlandVehicleRegister.Core;
+using System.Diagnostics;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        // Build SELECT Query with Multiple fields
+        public void TestSelectMultipleBuild()
+        {
+            QueryBuilder query = new QueryBuilder();
+            query.AddField(Field.Fields.merkki, "BMW");
+            query.AddField(Field.Fields.valmistenumero, "123456789");
+            query.Build(QueryBuilder.QueryType.Select);
+            string actual = query.QueryString;
+            string expected = "SELECT * FROM Ajoneuvo WHERE merkki='BMW', valmistenumero='123456789';";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        // Build SELECT Query with Single fields
+        public void TestSelectSingleBuild()
+        {
+            QueryBuilder query = new QueryBuilder();
+            query.AddField(Field.Fields.merkki, "BMW");
+            query.Build(QueryBuilder.QueryType.Select);
+            string actual = query.QueryString;
+            string expected = "SELECT * FROM Ajoneuvo WHERE merkki='BMW';";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        // Build SELECT Query with 0 fields
+        public void TestSelectBuild()
+        {
+            QueryBuilder query = new QueryBuilder();
+            query.Build(QueryBuilder.QueryType.Select);
+            string actual = query.QueryString;
+            string expected = "SELECT * FROM Ajoneuvo;";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
