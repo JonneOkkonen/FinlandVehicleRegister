9337d956a32a89ef1e3f645fa3a804bbdc37fb21
Jonne Okkonen <okkonen.jonne@gmail.com>
2019-04-18 21:47:09 +0300

    Core: Create VehicleAPI
    Create VehicleAPI that is use to download JSON string from API
    Create Unit Test for VehicleAPI
    Update QueryBuilder Tablename
    Add LIMIT 100 to QueryBuilder
    Update QueryBuilder UnitTests

---
+++ FinlandVehicleRegister/Core/VehicleAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinlandVehicleRegister.Core
+{
+    public static class VehicleAPI
+    {
+        private static readonly string APIKey = "9rP91EIhlOfiVVO5SZ1Bf2311U";
+        private static readonly string URL = $"https://www.jonneokkonen.com/api/ajoneuvorekisteri.php?apiKey={APIKey}&query=";
+        public static string LoadData(string query)
+        {
+            try
+            {
+                string result = "";
+                using (WebClient client = new WebClient())
+                {
+                    result = client.DownloadString(URL + query);
+                }
+                return result;
+            }catch
+            {
+                throw;
+            }
+        }
+    }
+}
