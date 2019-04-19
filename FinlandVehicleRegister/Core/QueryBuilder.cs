b1b1ca8420f443e1add9e108252cc03d4d70a07e
Jonne Okkonen <M2235@student.jamk.fi>
2019-04-19 23:34:42 +0300

    Core: Add GetOption to VehicleAPI
    Create method GetOption that gets all options from different tables.
    Like car colors, car fuel types...
    Create Option class to handle this data
    Add table selection and custom limit to QueryBuilder
    Create Unit Test for GetOption

--- FinlandVehicleRegister/Core/QueryBuilder.cs
+++ FinlandVehicleRegister/Core/QueryBuilder.cs
@@ -19,23 +19,35 @@ namespace FinlandVehicleRegister.Core
             }
         }
         private string queryString { get; set; }
-        private readonly string TableName = "Vehicle";
         private List<Field> fields = new List<Field>();
         public enum QueryType
         {
             Select
+        }
+
+        public enum Table
+        {
+            VAjoneuvoluokka,
+            VAjoneuvon_kaytto,
+            VAjoneuvoryhma,
+            VKayttovoima,
+            VKorityyppi,
+            VKunta,
+            VOhjaamotyyppi,
+            VVari,
+            Vehicle
         }
         
         /// <summary>
         /// Builds Basic Query based on query type and given fields
         /// </summary>
         /// <param name="type"></param>
-        public void Build(QueryType type)
+        public void Build(QueryType type, Table table = Table.Vehicle, int limit = 100)
         {
             switch(type)
             {
                 case QueryType.Select:
-                    queryString += $"SELECT * FROM {TableName}";
+                    queryString += $"SELECT * FROM {table}";
                     if (fields.Count != 0)
                     {
                         queryString += " WHERE ";
@@ -43,12 +55,12 @@ namespace FinlandVehicleRegister.Core
                         foreach (Field f in fields)
                         {
                             if (f == last)
-                                queryString += $"{f.FieldName}='{f.Value}' LIMIT 100;";
+                                queryString += $"{f.FieldName}='{f.Value}' LIMIT {limit};";
                             else
                                 queryString += $"{f.FieldName}='{f.Value}' AND ";
                         }
                     }
-                    else queryString += " LIMIT 100;";
+                    else queryString += $" LIMIT {limit};";
                     break;   
             }
         }
